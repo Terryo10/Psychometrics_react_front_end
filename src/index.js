4798714736167c84bbb8da0ea7b5ac7e09af0de2
Terryo10 <tapiwatererai@outlook.com>
2022-01-27 13:19:57 +0200

    styles setup

--- src/index.js
+++ src/index.js
@@ -3,15 +3,26 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import RootReducer from "./state/reducers/root_reducer";
+import { Provider } from "react-redux";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+const store = createStore(RootReducer, applyMiddleware(thunk));
+
+function render() {
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    document.getElementById("root")
+  );
+}
+
+store.subscribe(render);
+
+
+render()
+
 reportWebVitals();
